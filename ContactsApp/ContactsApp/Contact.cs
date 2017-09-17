9f1088e7e761c19c009f3bc7b6f6800d7063f485
nhatanph <31908615+nhatanph@users.noreply.github.com>
2017-09-17 12:27:18 +0700

    Sử dụng thuộc tính
    Dùng thuộc tính thay cho các hàm get/set. Đã sửa  trong lớp program cho phù hợp

--- ContactsApp/ContactsApp/Contact.cs
+++ ContactsApp/ContactsApp/Contact.cs
@@ -8,5 +8,41 @@ namespace ContactsApp
 {
     class Contact
     {
+        string ho;
+        public string Ho
+        {
+            get { return ho; }
+            set { ho = value; }
+        }
+
+        string ten;
+        public string Ten
+        {
+            get { return ten; }
+            set { ten = value; }
+        }
+
+        string diachi;
+        public string DiaChi
+        {
+            get { return diachi; }
+            set { diachi = value; }
+        }
+
+        string sdt;
+        public string SDT
+        {
+            get { return sdt; }
+            set { sdt = value; }
+        }
+
+        public Contact(string ho, string ten, string diachi, string sdt)
+        {
+            this.ho = ho;
+            this.ten = ten;
+            this.diachi = diachi;
+            this.sdt = sdt;
+        }
     }
-}
+        }
+
--- ContactsApp/ContactsApp/Program.cs
+++ ContactsApp/ContactsApp/Program.cs
@@ -10,6 +10,38 @@ namespace ContactsApp
     {
         static void Main(string[] args)
         {
+            List<Contact> danhba = new List<Contact>();
+            char r;
+            do
+            {
+                Console.WriteLine("\nMoi nhap thong tin lien lac:");
+                Console.Write("Moi nhap ho:");
+                string ho = Console.ReadLine();
+                Console.Write("Moi nhap ten:");
+                string ten = Console.ReadLine();
+                Console.Write("Moi nhap dia chi:");
+                string diachi = Console.ReadLine();
+                Console.Write("Moi nhap so dien thoai:");
+                string sdt = Console.ReadLine();
+
+                Contact co = new Contact(ho, ten, diachi, sdt);
+                danhba.Add(co);
+
+                Console.Write("Ban co muon tiep tuc nhap hay khong (y/n) ? ");
+                r = Console.ReadKey().KeyChar;
+            } while (r == 'y');
+
+            Console.WriteLine("\n\nDanh ba da nhap gom:");
+            foreach (Contact c in danhba)
+            {
+                Console.WriteLine("{0} - {1} - {2} - {3}",
+                                 c.Ho,
+                                 c.Ten,
+                                 c.DiaChi,
+                                 c.SDT);
+            }
+            Console.ReadLine();
         }
     }
 }
+    
